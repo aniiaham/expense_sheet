99c79004ccdd55a756a1a029e3369d4ac2751d09
aniiaham <aniya_axm@icloud.com>
2022-12-07 20:33:07 -0500

    added Expense form, New Expense files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ExpenseItem from "./components/Expenses/ExpenseItem";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -28,7 +28,7 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
