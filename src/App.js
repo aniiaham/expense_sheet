d182afc8f65958a88df7101fa6f27378670ba511
aniiaham <aniya_axm@icloud.com>
2022-12-10 16:45:56 -0500

    added ExpenseFilter file, draft

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
@@ -9,10 +10,7 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { id: "e2", 
-      title: "New TV",
-      amount: 799.49, 
-      date: new Date(2021, 2, 12) },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
     {
       id: "e3",
       title: "Car Insurance",
@@ -27,32 +25,13 @@ function App() {
     },
   ];
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense)
+    console.log("In App.js");
+    console.log(expense);
   };
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,24 +1,28 @@
 import React from "react";
 import ExpenseItem from "./ExpenseItem";
-import Card from "../components/UI/Card";
 import "../Expenses/Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter"
 
 function Expenses(props) {
   const { expenses } = props;
 
   return (
-    <Card>
-      {expenses.map((expense, index) => {
-        return (
-          <ExpenseItem
-            key={index}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <ExpensesFilter ExpensesFilter={ExpensesFilter} />
+      <Card>
+        {expenses.map((expense, index) => {
+          return (
+            <ExpenseItem
+              key={index}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 }
 export default Expenses;
